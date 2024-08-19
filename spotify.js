7eb2c84ccfd95d74762d4abb337a6dff85f119a7
Siddhu31124 <158408126+Siddhu31124@users.noreply.github.com>
2024-08-19 15:31:10 +0530

    Update spotify.js

--- spotify.js
+++ spotify.js
@@ -1,167 +1,196 @@
-let SongList=[
+
+        
+    
+        let SongList=[
     {
         image:"https://i.scdn.co/image/ab67616d0000b2734718e2b124f79258be7bc452",
         name:"StarBoy",
-        artist:"The Weekend"
+        artist:"The Weekend",
+        id:"starboy"
         
     },
     {
         image:"https://c.saavncdn.com/406/Ek-Tha-Raja-Hindi-2024-20240318125601-500x500.jpg",
         name:"Ek Tha Raja",
-        artist:"Badshasa"
+        artist:"Badshasa",
+        id:"raja"
     },
     {
         image:"https://i.scdn.co/image/ab67616d0000b273021d7017f73387b008eab271",
         name:"ney Veray",
-        artist:"Animal"
+        artist:"Animal",
+        id:"ney"
         
     },
     {
         image:"https://img.wynk.in/unsafe/200x200/filters:no_upscale():strip_exif():format(jpg)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_orchard/20230512070539_197188915704/1683896530964/resources/197188915704.jpg",
         name:"cheques",
-        artist:"Shubh"
+        artist:"Shubh",
+        id:"cheques"
         
     },
     {
         image:"https://upload.wikimedia.org/wikipedia/en/e/e6/The_Weeknd_-_Blinding_Lights.png",
         name:"Blinding Lights",
-        artist:"The Weekend"
+        artist:"The Weekend",
+        id:"lights"
         
     },
     {
         image:"https://i.scdn.co/image/ab67616d0000b2736404721c1943d5069f0805f3",
         name:"Tum Hi Ho",
-        artist:"Aashiqui 2"
+        artist:"Aashiqui 2",
+        id:"tum"
         
     },
     {
         image:"https://i.scdn.co/image/ab67616d0000b273d5f4378b1ffc9119fdc7306d",
         name:"Sajni",
-        artist:"Lapatha Ladies"
+        artist:"Lapatha Ladies",
+        id:"sajni"
         
     },
     {
         image:"https://i.scdn.co/image/ab67616d0000b2736d97b3dc154dfdbe2321fb5c",
         name:"Chuttamalle",
-        artist:"Devara"
+        artist:"Devara",
+        id:"chuttamalle"
         
     },
     {
         image:"https://c.saavncdn.com/584/V-Telugu-2020-20200828173314-500x500.jpg",
         name:"Manasu Maree",
-        artist:"V"
+        artist:"V",
+        id:"manasu"
         
     },
     {
         image:"https://assetscdn1.paytm.com/images/cinema/pushpa-2-(1)-(1)-ff7e1f10-fbfc-11ee-bfde-1dbbef51e33c.jpg",
         name:"PUSHPA PUSHPA",
-        artist:"Pushpa 2"
+        artist:"Pushpa 2",
+        id:"pushpa"
         
     },
     {
         image:"https://i.scdn.co/image/ab67616d0000b2738591a24a47e09545b988f9b2",
         name:"Fear Song",
-        artist:"Devara"
+        artist:"Devara",
+        id:"fear"
         
     },
     {
         image:"https://i2o.scdn.co/image/ab67706c0000cfa343d87044543b83bbbcf3b5bf",
         name:"Sitar",
-        artist:"Mr.Bachan"
+        artist:"Mr.Bachan",
+        id:"sitar"
         
     },
     {
         image:"https://i.scdn.co/image/ab67616d0000b2735fb9ec91608a996202583525",
         name:"Theam Of Kalki",
-        artist:"Kalki 2898AD"
+        artist:"Kalki 2898AD",
+        id:"kalki"
         
     },
     {
         image:"https://s.saregama.tech/image/c/fw_485/8/32/77/game_changer_tam_jaragandi_ott_1440_1711465462.jpg",
         name:"Jaragandi",
-        artist:"Game Changer"
+        artist:"Game Changer",
+        id:"jaragandi"
         
     },
     {
         image:"https://i.scdn.co/image/ab67616d0000b27385e12c19989a0cae9b5124f6",
         name:"Samavarthi",
-        artist:"Saripodhaa Sanivaaram"
+        artist:"Saripodhaa Sanivaaram",
+        id:"samavarthi"
         
     },
     {
         image:"https://c.saavncdn.com/385/Agnyaathavaasi-Telugu-2017-20180102061403-500x500.jpg",
         name:"Baitikochi chuste",
-        artist:"Agnyaathavaasi"
+        artist:"Agnyaathavaasi",
+        id:"chuste"
         
     },
     {
         image:"https://c.saavncdn.com/623/Jersey-Telugu-2019-20190427102305-500x500.jpg",
         name:"Adhento Gaani",
-        artist:"Jersey"
+        artist:"Jersey",
+        id:"adhento"
         
     },
     {
         image:"https://c.saavncdn.com/244/3-Telugu-2012-500x500.jpg",
         name:"Kolaveri Di",
-        artist:"3"
+        artist:"3",
+        id:"kolaveri"
         
     },
     {
         image:"https://c.saavncdn.com/026/Chaleya-From-Jawan-Hindi-2023-20230814014337-500x500.jpg",
         name:"Chaleya",
-        artist:"Jawan"
+        artist:"Jawan",
+        id:"chaleya"
         
     },
     {
         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHS3g5vpIMKPtm90WUyG7h-6KHtiEUx1uiuA&s",
         name:"Aaj ki Rath",
-        artist:"Stree 2"
+        artist:"Stree 2",
+        id:"aaj"
         
     },
     {
         image:"https://i.scdn.co/image/ab67616d00001e02281650a8e8c5d04658d31ac1",
         name:"Ghungroo",
-        artist:"War"
+        artist:"War",
+        id:"ghungroo"
         
     },
     {
         image:"https://c.saavncdn.com/807/Kabir-Singh-Hindi-2019-20240131131003-500x500.jpg",
         name:"Bekayali (Kabir Sing)",
-        artist:"Kabir Sing"
+        artist:"Kabir Sing",
+        id:"kabir"
         
     },
     {
         image:"https://i.scdn.co/image/ab67616d00001e02707ea5b8023ac77d31756ed4",
         name:"BADTAMEEZ DIL",
-        artist:"Hey Jawani He Diwani"
+        artist:"Hey Jawani He Diwani",
+        id:"badtameez"
         
     },
     {
         image:"https://i1.sndcdn.com/artworks-000126060865-qlbvre-t500x500.jpg",
         name:"Hamari Adhuri Kahani",
-        artist:"Hamari Adhuri Kahani"
+        artist:"Hamari Adhuri Kahani",
+        id:"adhuri"
         
     },
     {
         image:"https://www.pagalworld.com.so/uploads/thumb/sft43/21347_4.jpg",
         name:"Tauba Tauba",
-        artist:"BadNews"
+        artist:"BadNews",
+        id:"tauba"
         
     },
     {
         image:"https://pagalfree.com/images/128Kesariya%20-%20Brahmastra%20128%20Kbps.jpg",
         name:"Kesariya",
-        artist:"Brahmastra"
+        artist:"Brahmastra",
+        id:"kesariya"
         
     },
     {
         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9O_iBlZKkH9Rn5zQIbG_QAR2MWSt9I9h7_Q&s",
         name:"What Jumka",
-        artist:"Rocky Aur Rani Kii Prem Kahaani"
+        artist:"Rocky Aur Rani Kii Prem Kahaani",
+        id:"what"
         
     },
-
-    
+   
 ]
 let PlayerBar=document.getElementById("playBar")
 let songs=[]
@@ -194,6 +223,7 @@ main()
 let index
 let song
 let indexofSong
+let nameSong
 let songname=document.getElementById("SongName")
 let songartist=document.getElementById("SongArtist")
 let img=document.getElementById("imageOfPlaybar")
@@ -208,6 +238,7 @@ function time(name){
             return false
         }
     })
+    console.log(SongList[index])
     img.setAttribute("src",SongList[index].image)
     img.classList.add("playbarImg")
     songname.textContent=SongList[index].name
@@ -242,7 +273,6 @@ function selectSong(name) {
     indexofSong=songs.findIndex((items)=>{
         if(items.toLowerCase().includes(name)){ return true}
     })
-    console.log(indexofSong)
     for (let i of songs){
         if(i.toLowerCase().includes(name)){
             song = new Audio(i);
@@ -251,16 +281,6 @@ function selectSong(name) {
             document.getElementById('volumeControl').addEventListener('input', function() {
             song.volume = document.getElementById('volumeControl').value;});
             songtime(song)
-            // song.addEventListener("timeupdate",()=>{
-            // presenttime.textContent=formatTime(song.currentTime)
-            // totaltime.textContent=formatTime(song.duration)
-            // presenttime1.textContent=formatTime(song.currentTime)
-            // totaltime1.textContent=formatTime(song.duration)
-            // document.querySelector(".cricle").style.left=(song.currentTime/song.duration)*100+"%"})
-            // document.querySelector(".seekbar").addEventListener("click",(event)=>{
-            //     let SongtimePercent=(event.offsetX/event.target.getBoundingClientRect().width)*100
-            //     document.querySelector(".cricle").style.left=SongtimePercent+"%"
-            //     song.currentTime=(song.duration*SongtimePercent)/100})
             previousSong=song
         }
     } 
@@ -296,15 +316,17 @@ document.getElementById("centerid1").addEventListener("click",()=>{
 document.getElementById("rightid").addEventListener("click",()=>{
     if(index<SongList.length-1){
         previousSong.pause()
-        img.setAttribute("src",SongList[index+1].image)
-        img.classList.add("playbarImg")
-        songname.textContent=SongList[index+1].name
-        songartist.textContent=SongList[index+1].artist
-        mini.textContent=SongList[index+1].name
         song=new Audio(songs[indexofSong+1])
         song.play()
         songtime(song)
-        console.log(indexofSong,index)
+        let updatedsonglist
+        for (let i of SongList){
+            if(songs[indexofSong+1].toLowerCase().includes(i.id)){
+                updatedsonglist=(i.id)
+                break;
+            }
+        }
+        time(updatedsonglist)
         previousSong=song
         index=index+1  
         indexofSong=indexofSong+1
@@ -313,14 +335,17 @@ document.getElementById("rightid").addEventListener("click",()=>{
         index=0
         indexofSong=0
         previousSong.pause()
-        img.setAttribute("src",SongList[index].image)
-        img.classList.add("playbarImg")
-        songname.textContent=SongList[index].name
-        songartist.textContent=SongList[index].artist
-        mini.textContent=SongList[index].name 
         song=new Audio(songs[indexofSong])
         song.play()
         songtime(song)
+        let updatedsonglist
+        for (let i of SongList){
+            if(songs[indexofSong].toLowerCase().includes(i.id)){
+                updatedsonglist=(i.id)
+                break;
+            }
+        }
+        time(updatedsonglist)
         previousSong=song
     }
     
@@ -328,17 +353,17 @@ document.getElementById("rightid").addEventListener("click",()=>{
 document.getElementById("rightid1").addEventListener("click",()=>{
     if(index<SongList.length-1){
         previousSong.pause()
-        console.log(SongList.length)
-        console.log(index)
-        img.setAttribute("src",SongList[index+1].image)
-        img.classList.add("playbarImg")
-        songname.textContent=SongList[index+1].name
-        songartist.textContent=SongList[index+1].artist
-        mini.textContent=SongList[index+1].name
-        index=index+1
         song=new Audio(songs[indexofSong+1])
         song.play()
         songtime(song)
+        let updatedsonglist
+        for (let i of SongList){
+            if(songs[indexofSong+1].toLowerCase().includes(i.id)){
+                updatedsonglist=(i.id)
+                break;
+            }
+        }
+        time(updatedsonglist)
         previousSong=song
         index=index+1  
         indexofSong=indexofSong+1  
@@ -347,77 +372,96 @@ document.getElementById("rightid1").addEventListener("click",()=>{
         index=0
         indexofSong=0
         previousSong.pause()
-        img.setAttribute("src",SongList[index].image)
-        img.classList.add("playbarImg")
-        songname.textContent=SongList[index].name
-        songartist.textContent=SongList[index].artist
-        mini.textContent=SongList[index].name
         song=new Audio(songs[indexofSong])
         song.play()
         songtime(song)
-        previousSong=song 
+        let updatedsonglist
+        for (let i of SongList){
+            if(songs[indexofSong].toLowerCase().includes(i.id)){
+                updatedsonglist=(i.id)
+                break;
+            }
+        }
+        time(updatedsonglist)
+        previousSong=song
     }
-    
     })
     document.getElementById("leftid").addEventListener("click",()=>{
         if(index>0 && indexofSong>0){
-            console.log(indexofSong)
-            console.log(index)
             previousSong.pause()
-            img.setAttribute("src",SongList[index-1].image)
-            img.classList.add("playbarImg")
-            songname.textContent=SongList[index-1].name
-            songartist.textContent=SongList[index-1].artist
-            mini.textContent=SongList[index-1].name 
-            song=new Audio(songs[indexofSong-1])
-            song.play()
-            songtime(song)
-            previousSong=song
-            index=index-1  
-            indexofSong=indexofSong-1 
+        song=new Audio(songs[indexofSong-1])
+        song.play()
+        songtime(song)
+        let updatedsonglist
+        for (let i of SongList){
+            if(songs[indexofSong-1].toLowerCase().includes(i.id)){
+                updatedsonglist=(i.id)
+                break;
+            }
+        }
+        time(updatedsonglist)
+        previousSong=song
+        index=index-1  
+        indexofSong=indexofSong-1 
         }
         else{
-            console.log("over")
             previousSong.pause()
             index=SongList.length-1
             indexofSong=songs.length-1
-            img.setAttribute("src",SongList[index].image)
-            img.classList.add("playbarImg")
-            songname.textContent=SongList[index].name
-            songartist.textContent=SongList[index].artist
-            mini.textContent=SongList[index].name 
             song=new Audio(songs[indexofSong])
-            song.play()
-            songtime(song)
+        song.play()
+        songtime(song)
+        let updatedsonglist
+        for (let i of SongList){
+            if(songs[indexofSong].toLowerCase().includes(i.id)){
+                updatedsonglist=(i.id)
+                break;
+            }
+        }
+        time(updatedsonglist)
+        previousSong=song
             previousSong=song 
         }
     })
     document.getElementById("leftid1").addEventListener("click",()=>{
-        if(index>0){
+        if(index>0 && indexofSong>0){
             previousSong.pause()
-            img.setAttribute("src",SongList[index-1].image)
-            img.classList.add("playbarImg")
-            songname.textContent=SongList[index-1].name
-            songartist.textContent=SongList[index-1].artist
-            mini.textContent=SongList[index-1].name
-            song.play()
-            songtime(song)
-            previousSong=song
-            index=index-1  
-            indexofSong=indexofSong-1  
+        song=new Audio(songs[indexofSong-1])
+        song.play()
+        songtime(song)
+        let updatedsonglist
+        for (let i of SongList){
+            if(songs[indexofSong-1].toLowerCase().includes(i.id)){
+                updatedsonglist=(i.id)
+                break;
+            }
+        }
+        time(updatedsonglist)
+        previousSong=song
+        index=index-1  
+        indexofSong=indexofSong-1 
         }
         else{
             previousSong.pause()
             index=SongList.length-1
             indexofSong=songs.length-1
-            img.setAttribute("src",SongList[index].image)
-            img.classList.add("playbarImg")
-            songname.textContent=SongList[index].name
-            songartist.textContent=SongList[index].artist
-            mini.textContent=SongList[index].name 
             song=new Audio(songs[indexofSong])
-            song.play()
-            songtime(song)
+        song.play()
+        songtime(song)
+        let updatedsonglist
+        for (let i of SongList){
+            if(songs[indexofSong].toLowerCase().includes(i.id)){
+                updatedsonglist=(i.id)
+                break;
+            }
+        }
+        time(updatedsonglist)
+        previousSong=song
             previousSong=song 
         }
     })
+
+
+
+
+
